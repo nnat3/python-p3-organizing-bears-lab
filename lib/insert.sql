9036e66d0f6273477403433f5c5bc651fcde4741
Neathan Kungu <natekungu857@gmail.com>
2025-12-05 11:08:26 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 10, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'brown', 'friendly', 1),
+('Sergeant Brown', 19, 'M', 'brown', 'strict', 1),
+('Melissa', 13, 'F', 'blonde', 'sweet', 1),
+('Grinch', 2, 'M', 'green', 'grumpy', 1),
+('Wendy', 6, 'F', 'white', 'gentle', 1),
+(NULL, 15, 'M', 'gray', 'mysterious', 1);
